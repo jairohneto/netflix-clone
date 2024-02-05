bb00350729be75e5d335c00d2f54439bc23dfea0
Jairo <jairohneto@gmail.com>
2024-02-05 20:26:55 -0300

    netflix commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import './App.css'
 import Tmdb from "./Tmdb";
-import MovieRow from "./components/movieRow";
+import MovieRow from "./components/MovieRow"
 import FeaturedMovie from "./components/FeaturedMovie";
 import Header from "./components/Header";
 
