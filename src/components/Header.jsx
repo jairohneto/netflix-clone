d9b7eb4e7c07f5a9a1667241df9458dd449ae4ae
Jairo <jairohneto@gmail.com>
2024-02-06 16:51:50 -0300

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ export default ({black}) => {
     <header className={black ? 'black' :  ''}>
       <div className="header--logo">
         <a href="/">
-          <img src="src/assets/logonetflix.png" alt="Netflix" />
+          <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="Netflix" />
         </a>
       </div>
       <div className="header--user">
         <a href="/">
-          <img src="src/assets/netflixavatar.png" alt="User" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="User" />
         </a>
       </div>
     </header>
